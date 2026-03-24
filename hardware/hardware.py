aeb631a2b1d846e18187134e0d8c9734ac081110
hminh1805 <hoangminh.truong1805@gmail.com>
2026-03-24 23:30:31 +0700

    thêm CẢNH BÁO cho hardware

--- hardware/hardware.py
+++ hardware/hardware.py
@@ -4,6 +4,7 @@ button_a.on_pressed = None
 button_b.on_pressed = None
 button_a.on_pressed_ab = button_b.on_pressed_ab = -1
 from aiot_rgbled import RGBLed
+import music
 from machine import RTC
 import ntptime
 import time
@@ -15,32 +16,39 @@ from aiot_dht20 import DHT20
 tiny_rgb = RGBLed(pin13.pin, 4)
 
 def on_mqtt_message_receive_callback__LED_(th_C3_B4ng_tin):
-  global SM, RT, RH, LUX, serial
+  global serial, RT, RH, LUX, SM
   if th_C3_B4ng_tin == '1':
     tiny_rgb.show(0, hex_to_rgb('#ff0000'))
   else:
     tiny_rgb.show(0, hex_to_rgb('#000000'))
 
 def on_mqtt_message_receive_callback__MAYBOM_(th_C3_B4ng_tin):
-  global SM, RT, RH, LUX, serial
+  global serial, RT, RH, LUX, SM
   if th_C3_B4ng_tin == '1':
     pin14.write_analog(round(translate(70, 0, 100, 0, 1023)))
   else:
     pin14.write_analog(round(translate(0, 0, 100, 0, 1023)))
 
 def on_mqtt_message_receive_callback__SERVO_(th_C3_B4ng_tin):
-  global SM, RT, RH, LUX, serial
+  global serial, RT, RH, LUX, SM
   if th_C3_B4ng_tin == '1':
     pin15.servo_write(90)
   else:
     pin15.servo_write(0)
 
 def on_mqtt_message_receive_callback__QUAT_(th_C3_B4ng_tin):
-  global SM, RT, RH, LUX, serial
+  global serial, RT, RH, LUX, SM
   if th_C3_B4ng_tin == '1':
     pin16.write_analog(round(translate(70, 0, 100, 0, 1023)))
   else:
     pin16.write_analog(round(translate(0, 0, 100, 0, 1023)))
+
+def on_mqtt_message_receive_callback__CANHBAO_(th_C3_B4ng_tin):
+  global serial, RT, RH, LUX, SM
+  if th_C3_B4ng_tin == '1':
+    music.play(music.POWER_UP, wait=False)
+  else:
+    music.stop()
 
 # Mô tả hàm này...
 def _C4_90_C4_82NG_K_C3_9D_SERVER():
@@ -49,6 +57,7 @@ def _C4_90_C4_82NG_K_C3_9D_SERVER():
   mqtt.on_receive_message('MAYBOM', on_mqtt_message_receive_callback__MAYBOM_)
   mqtt.on_receive_message('SERVO', on_mqtt_message_receive_callback__SERVO_)
   mqtt.on_receive_message('QUAT', on_mqtt_message_receive_callback__QUAT_)
+  mqtt.on_receive_message('CANHBAO', on_mqtt_message_receive_callback__CANHBAO_)
 
 aiot_lcd1602 = LCD1602()
 
@@ -57,7 +66,7 @@ event_manager.reset()
 aiot_dht20 = DHT20()
 
 def on_event_timer_callback_U_h_W_p_G():
-  global th_C3_B4ng_tin, SM, RT, RH, LUX, serial
+  global th_C3_B4ng_tin, serial, RT, RH, LUX, SM
   aiot_dht20.read_dht20()
   RT = aiot_dht20.dht20_temperature()
   RH = aiot_dht20.dht20_humidity()
@@ -95,13 +104,13 @@ def on_event_timer_callback_U_h_W_p_G():
 event_manager.add_timer_event(5000, on_event_timer_callback_U_h_W_p_G)
 
 def on_button_a_pressed():
-  global th_C3_B4ng_tin, SM, RT, RH, LUX, serial
+  global th_C3_B4ng_tin, serial, RT, RH, LUX, SM
   serial = 1
 
 button_a.on_pressed = on_button_a_pressed
 
 def on_button_b_pressed():
-  global th_C3_B4ng_tin, SM, RT, RH, LUX, serial
+  global th_C3_B4ng_tin, serial, RT, RH, LUX, SM
   serial = 0
 
 button_b.on_pressed = on_button_b_pressed
